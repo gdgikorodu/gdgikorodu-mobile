f7f8705b0b7f481dcf6cc2bc7384f2f23c673391
Mastersam <abadasamuelosp@gmail.com>
2020-02-16 07:04:59 -0800

    some updates

--- lib/screens/blog.dart
+++ lib/screens/blog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:gdgikorodu/utils/dev_scaffold.dart';
 
 class BlogPage extends StatelessWidget {
@@ -8,11 +7,8 @@ class BlogPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DevScaffold(
-      body: WebviewScaffold(
-        url: 'https://www.meetup.com/gdg_ikorodu/',
-        hidden: true,
-        clearCache: false,
-        appCacheEnabled: true,
+      body: Center(
+        child: Text('Coming Soon'),
       ),
       title: "Blog",
     );
