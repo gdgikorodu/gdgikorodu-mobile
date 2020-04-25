d31b8ffac6649ead95f6b5511645e924d1fbe460
Mastersam <abadasamuelosp@gmail.com>
2020-04-25 13:40:44 -0700

    fetch events

---
+++ lib/network/eventdata.dart
@@ -0,0 +1,31 @@
+import 'package:http/http.dart' as http;
+import 'package:gdgikorodu/models/event_data.dart';
+import 'dart:convert';
+
+class NewsForCorona {
+  List<Event> events = [];
+
+  Future<void> getEvents() async {
+    String url =
+        "https://us-central1-gdgikorodu-event-details-api.cloudfunctions.net/webApi/api/v1/events";
+
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["data"].forEach((element) {
+        if (element['name'] != null && element['details'] != null) {
+          Event event = Event(
+            name: element['name'],
+            details: element['details'],
+            date: element['date'],
+            location: element['location'],
+          );
+          events.add(event);
+        }
+      });
+    }
+  }
+}
